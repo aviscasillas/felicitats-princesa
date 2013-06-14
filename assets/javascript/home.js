b801ad267c5a1e120cd490a2928557b8569cf08b
Albert Viscasillas Campanyà <albert@wuaki.tv>
2013-06-14 18:02:01 +0200

    Functional implementation

--- assets/javascript/home.js
+++ assets/javascript/home.js
@@ -4,23 +4,37 @@ function Home() {
   this.initialize = function(){
     me.page = 0;
     me.show();
-  }
+  };
 
-  this.next = function() {
+  this.avpage = function() {
     me.page ++;
     me.show();
-  }
+  };
 
   this.show = function() {
     $.ajax({
-      type: "GET",
-      url: "templates/" + me.page + ".html",
+      type: 'GET',
+      url: 'templates/' + me.page + '.html',
       success: function(data) {
-        alert(data);
-        $('content').html(data);
+        $('#content').html(data);
+        setAction();
       }
     });
-  }
+  };
+
+  var setAction = function() {
+    $('[name="response"]').on('click', checkAction);
+  };
+
+  var checkAction = function() {
+    var ok_val = $('[name="ok_val"]').val();
+    var checked_val = $(this).attr('value');
+    if(ok_val == checked_val) {
+      me.avpage();
+    } else {
+      me.initialize();
+    }
+  };
 
   this.initialize();
 }
