0351778adf578344dfb8d6ae148ba9c4f05255c3
Albert Viscasillas <aviscasillas@gmail.com>
2013-06-15 19:55:38 +0200

    Playing on heroku

--- templates/0.php
+++ templates/0.php
@@ -4,14 +4,7 @@
     "Sí" => 1,
     "No" => 2
 	);
-	template("A hores d'ara ja deus tenir les teves CROGS color violeta...<br/><br/> Estas contenta?", $options, 1);
+	$msg = "A hores d'ara ja deus tenir les teves CROGS color violeta...<br/><br/> Estas contenta?";
+	
+	template($msg, $options, 1);
 ?>
-<p>
-  A hores d'ara ja deus tenir les teves CROGS color violeta...<br/><br/>
-  Estas contenta?
-</p>
-
-<a name="response" value="1">Sí</a>
-<a name="response" value="0">No</a>
-
-<input type="hidden" name="ok_val" value="1" />
--- templates/1.php
+++ templates/1.php
@@ -1,10 +1,12 @@
-<p>
-  Perfecte!! Mencanta que siguis feliç :)<br/>
-  Però, encara vull que siguis més feliç, i per aquest motiu t'he preparat un altre regal.<br/><br/>
-  Vols saber on és?
-</p>
+<?php
+	include "helper.php";
+	$options = array(
+    "Sí" => 1,
+    "No" => 2
+	);
+	$msg = "Perfecte!! Mencanta que siguis feliç :)<br/>
+  				Però, encara vull que siguis més feliç, i per aquest motiu t'he preparat un altre regal.<br/><br/>
+  				Vols saber on és?";
 
-<a name="response" value="1">Sí</a>
-<a name="response" value="0">No</a>
-
-<input type="hidden" name="ok_val" value="1" />
+	template($msg, $options, 1);
+?>
