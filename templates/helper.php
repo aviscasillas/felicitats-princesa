572f69c5505de634af3750627f2f779ea7d50ca1
Albert Viscasillas <aviscasillas@gmail.com>
2013-06-15 19:48:26 +0200

    Playing on heroku

--- templates/helper.php
+++ templates/helper.php
@@ -1,6 +1,9 @@
 <?php
 function template($msg, $options, $ok_val) {
- echo '<p>' . $msg . "</p>";
- echo '<input type="text" name="ok_val" value="' . $ok_val . '" />';
+	echo '<p>' . $msg . "</p>";
+ 	foreach ($options as $label => $value) {
+    echo '<a name="response" value="' . $value . '">' . $label . '</a>'
+	}
+ 	echo '<input type="hidden" name="ok_val" value="' . $ok_val . '" />';
 }
 ?>
