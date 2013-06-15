b16370ad81bcd6c68dcc1d1492bbfcd9bceffc68
Albert Viscasillas <aviscasillas@gmail.com>
2013-06-15 19:30:09 +0200

    Playing on heroku

--- templates/helper.php
+++ templates/helper.php
@@ -1,6 +1,6 @@
 <?php
 function template($msg, $options, $ok_val) {
  echo '<p>' . $msg . "</p>";
- echo '<input type="text" name="ok_val" value="' . $ok_val . '" />'
+ echo '<input type="text" name="ok_val" value="' . $ok_val . '" />';
 }
 ?>
