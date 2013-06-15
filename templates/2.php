e4f3c8c65e96825f4e54e46fee1da9da0ceafbd3
Albert Viscasillas <aviscasillas@gmail.com>
2013-06-15 19:58:16 +0200

    Playing on heroku

--- templates/2.php
+++ templates/2.php
@@ -1,8 +1,10 @@
-<p>
-  O la la!!!
-</p>
+<?php
+	include "helper.php";
+	$options = array(
+    "Sí" => 1,
+    "No" => 2
+	);
+	$msg = "O la la!!";
 
-<a name="response" value="1">Sí</a>
-<a name="response" value="0">No</a>
-
-<input type="hidden" name="ok_val" value="1" />
+	template($msg, $options, 1);
+?>
