7be1e4d4571c15069d81458abc44c4866462595b
Albert Viscasillas <aviscasillas@gmail.com>
2013-06-15 20:40:04 +0200

    Playing on heroku

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <link rel="stylesheet" href="assets/stylesheet/home.css">
     <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
     <script type="text/javascript" src="assets/javascript/home.js"></script>
