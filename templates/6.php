d935638118da23c61b12e45fb0fd7d3f2671a216
Albert Viscasillas Campanyà <albert@wuaki.tv>
2013-06-19 14:20:50 +0200

    Writting templates

--- templates/6.php
+++ templates/6.php
@@ -11,7 +11,7 @@
   $msg = "Sí, tu per mi ets la lluna plena... em dones llum quan tot
           és fosc i m'ipnotitzes quan et miro.<br/><br/>
           El regal està en un lloc tancat, protegit de la pluja però, no de la calor.<br/><br/>
-          A part de la lluna per mi també ets:";
+          A a més de la lluna per mi també ets:";
 
   template($msg, $options, 4);
 ?>
